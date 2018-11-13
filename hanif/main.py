38c7b4ef3c032328dbfec625c86d166336aff7dd
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-13 00:22:24 -0500

    reduced the neurons density and added JOURNAL.MD

--- hanif/main.py
+++ hanif/main.py
@@ -38,11 +38,9 @@ cv_scores = []
 for train, test in k_fold.split(X_joined, Y):
     # create model
     model = Sequential()
-    model.add(Dense(9, input_dim=14, activation='relu'))
-    model.add(Dense(18, activation='relu'))
-    model.add(Dense(36, activation='relu'))
-    model.add(Dense(18, activation='relu'))
-    model.add(Dense(9, activation='relu'))
+    model.add(Dense(14, input_dim=14, activation='relu'))
+    model.add(Dense(7, activation='relu'))
+    model.add(Dense(3, activation='relu'))
     model.add(Dense(1, activation='relu'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
