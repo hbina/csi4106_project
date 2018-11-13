2be11be6ae52dc1dce037ec98e365a08f57e22ad
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-12 22:58:54 -0500

    evaluation method taken from

--- hanif/main.py
+++ hanif/main.py
@@ -1,58 +1,57 @@
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-from keras.layers import Dense
-from keras.models import Sequential
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.impute import SimpleImputer
 import numpy as np
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras import Sequential
+from keras.layers import Dense
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import OneHotEncoder
 
-print("opening dataset file....")
 X = pd.read_csv('../dataset/train.csv')
-print("obtaining the annotated result...")
+
+irrelevant_features = ['Cabin', 'Embarked', 'Ticket', 'PassengerId']
+for irr_f in irrelevant_features:
+    X.pop(irr_f)
 Y = X.pop('Survived').values
-print("splitting dataset into training and test")
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
 
-print("shape of X", X_train.shape)
-print("shape of Y", y_test.shape)
-
-print("performing one hot encoding on the input X")
 one_hot_encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
-feature_set = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+feature_set = ['Pclass', 'Sex', 'Parch']
+X_train_sf = X[feature_set].copy()
+one_hot_encoder.fit(X_train_sf)
 
-print("filtering out irrelevant features...")
-X_train_sf = X_train[feature_set].copy()
-X_test_sf = X_test[feature_set].copy()
+feature_names = one_hot_encoder.get_feature_names()
+
+X_train_sf_encoded = one_hot_encoder.transform(X_train_sf)
+X_filtered = X[['Fare', 'Age']]
 
 # Imputer...replaces NaN object with the mean...
 # Obtained from https://stackoverflow.com/a/30319249
-# Create our imputer to replace missing values with the mean e.g.
-print("using Imputer to convert unknown values to mean")
+# Create our Imputer to replace missing values with the mean e.g.
 imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp = imp.fit(X_train_sf)
+imp = imp.fit(X_filtered)
 # Impute our data, then train
-X_train_imp = imp.transform(X_train_sf)
-print("imputed training set")
-print(X_train_imp)
 
-one_hot_encoder.fit(X_train_imp)
+X_train_imp = imp.transform(X_filtered)
+X_joined = np.concatenate([X_train_imp, X_train_sf_encoded], axis=1)
 
-feature_names = one_hot_encoder.get_feature_names()
-print("the following are the features that have been encoded")
-print(feature_names)
-
-X_train_sf_encoded = one_hot_encoder.transform(X_train_imp)
-X_test_sf_encoded = one_hot_encoder.transform(X_test_sf)
-
-model = Sequential()
-model.add(Dense(9, input_dim=9, kernel_initializer='uniform', activation='sigmoid'))
-model.add(Dense(18, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(36, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(18, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(9, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(1, kernel_initializer='uniform', activation='relu'))
-model.summary()
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train_sf_encoded, y_train,
-          epochs=20)
+print(X_joined.shape)
+seed = 7
+k_fold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+cv_scores = []
+for train, test in k_fold.split(X_joined, Y):
+    # create model
+    model = Sequential()
+    model.add(Dense(9, input_dim=14, kernel_initializer='uniform', activation='sigmoid'))
+    model.add(Dense(18, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(36, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(18, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(9, kernel_initializer='uniform', activation='relu'))
+    model.add(Dense(1, kernel_initializer='uniform', activation='relu'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # Fit the model
+    model.fit(X_joined[train], Y[train], epochs=150, batch_size=10, verbose=0)
+    # evaluate the model
+    scores = model.evaluate(X_joined[test], Y[test], verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    cv_scores.append(scores[1] * 100)
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cv_scores), np.std(cv_scores)))
