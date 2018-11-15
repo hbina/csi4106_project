8d910f4fb2d8fce1b5ebc52a2c45fed746536188
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-14 21:59:33 -0500

    added tensor board support and uhhh

--- hanif/hanif_main.py
+++ hanif/hanif_main.py
@@ -1,7 +1,10 @@
+from time import time
 import numpy as np
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import sklearn
+import keras as ks
 from keras import Sequential
+from keras.callbacks import TensorBoard
 from keras.layers import Dense
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import StratifiedKFold
@@ -52,13 +55,19 @@ for train, test in k_fold.split(x_train_preprocessed, Y_train):
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(x_train_preprocessed[train], Y_train[train], epochs=100, verbose=0)
+    tbCallBack = ks.callbacks.TensorBoard(log_dir='logs', histogram_freq=0,
+                             write_graph=True, write_images=True)
+    model.fit(x_train_preprocessed[train], Y_train[train], epochs=100, verbose=1, callbacks=[tbCallBack])
     # evaluate the model
-    scores = model.evaluate(x_train_preprocessed[test], Y_train[test], verbose=0)
+    scores = model.evaluate(x_train_preprocessed[test], Y_train[test], verbose=1)
     print("scores:\n", scores)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     cv_scores.append(scores[1] * 100)
     if scores[1] > 0.6:
+        print("summary:")
         model.summary()
-        # print(model.predict(X_test_preprocessed))
+        print("weights:")
+        print(model.get_weights())
+        print("test dataset predictions:")
+        print(model.predict(X_test_preprocessed))
 print(np.mean(cv_scores), "% +- ", np.std(cv_scores), "%")
