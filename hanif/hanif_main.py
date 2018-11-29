708ff039305e7656884ad14cde61c8d41a7e8296
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-28 23:15:41 -0500

    removed unnecessary imports

--- hanif/hanif_main.py
+++ hanif/hanif_main.py
@@ -1,10 +1,8 @@
-from time import time
+import keras as ks
 import numpy as np
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import sklearn
-import keras as ks
 from keras import Sequential
-from keras.callbacks import TensorBoard
 from keras.layers import Dense
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import StratifiedKFold
